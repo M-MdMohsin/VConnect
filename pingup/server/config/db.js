3af065c7424faad32db3685e23f829508706389a
M-MdMohsin <emceeee6@gmail.com>
2026-02-12 18:13:38 +0530

    small error fixed

--- pingup/server/config/db.js
+++ pingup/server/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         mongoose.connection.on('connected', ()=> console.log('Database Connected'))
-        await mongoose.connect(`${process.env.MONGODB_URL}/VConnect`)
+        await mongoose.connect(`${process.env.MONGODB_URL}/vconnect`)
     }
     catch (error) {
         console.log(error.message)
--- pingup/server/inngest/index.js
+++ pingup/server/inngest/index.js
@@ -10,7 +10,8 @@ const syncUserCreation = inngest.createFunction(
     {event: 'clerk/user.created'},
     async ({event})=> {
         const {id, first_name, last_name, email_addresses, image_url} = event.data
-        let username = email_addresses[0].email_address.split('@')[0]
+        const email = email_addresses[0].email_address
+        let username = email.split('@')[0]
 
         // Check availability of username
         const user = await User.findOne({username})
@@ -41,7 +42,7 @@ const syncUserUpdation = inngest.createFunction(
         const {id, first_name, last_name, email_addresses, image_url} = event.data
         
         const updatedUserData = {
-            email: email_addresses[0].email_addresses,
+            email: email_addresses[0].email_address,
             full_name: first_name + ' ' + last_name,
             profile_picture: image_url
         }
