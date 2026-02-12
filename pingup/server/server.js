c5889e39c30e9b189e00f195d1fa222147e4fc0d
M-MdMohsin <emceeee6@gmail.com>
2026-02-12 13:49:09 +0530

    Initializing ingest for backend and user-deletion,creation,updation functions for mongodb database

--- pingup/server/server.js
+++ pingup/server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './config/db.js';
+import { inngest,functions } from './ingest/index.js';
 
 const app = express();
 
@@ -11,6 +12,8 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res)=> res.send('server is running'))
+
+app.use('/api/ingest', serve({client: inngest, functions}))   //Integrating ingest into project
 
 const PORT = process.env.PORT || 4000;
 
