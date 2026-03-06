6d9f5cc092301feec7c043639820a3052228ace4
M-MdMohsin <emceeee6@gmail.com>
2026-03-06 19:55:46 +0530

    minor change in backend

--- pingup/server/routes/messageRoutes.js
+++ pingup/server/routes/messageRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getChatMessages, sendMessage, sseController } from '../controllers/messageController'
+import { getChatMessages, sendMessage, sseController } from '../controllers/messageController.js'
 import upload from '../config/multer.js'
 import { protect } from '../middleware/auth.js'
 
